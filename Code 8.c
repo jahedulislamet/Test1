fba0314fd33f7243f811908db408cbfb5a843d58
jahedulislamet <jahedulislamiiucet251060@gmail.com>
2025-03-21 20:44:29 +0600

    Create Code 8.c

---
+++ Code 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+  double 1, w, area;
+
+printf("Enter the length of the rectangle :");
+scanf("%1f", &1)
+  
+printf ("Enter the width of the rectangle :");
+scanf("%1f", &w)
+  
+area =1 * w;
+
+printf("The area of rectangle :%1f",area);
+
+return 0;
+}
