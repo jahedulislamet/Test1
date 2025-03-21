e6bf1cc22774aedb3ca25c1291f34c591415c7d1
jahedulislamet <jahedulislamiiucet251060@gmail.com>
2025-03-21 20:34:10 +0600

    Create Code 7.c

---
+++ Code 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+  double n, result ;
+
+printf("Enter a number :");
+scanf("%1f", &n);
+
+result = sqrt(n);
+
+printf("square root =%1f", result);
+}
